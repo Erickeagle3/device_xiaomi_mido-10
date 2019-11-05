f1b88ec75720a9663dc629d48a3053ca8f6748cb
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-11-05 13:32:04 +0530

    mido: add prebuilt via

--- device.mk
+++ device.mk
@@ -426,6 +426,13 @@ PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl \
     libwfdaac
+    CarrierConfig
+
+# Via
+PRODUCT_PACKAGES += \
+    Via
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
