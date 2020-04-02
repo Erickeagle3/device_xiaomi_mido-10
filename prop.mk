97bcf5010f6c60aa39ff20567ec3ccf1d2f21af6
xNombre <xNombre@users.noreply.github.com>
2020-04-02 16:00:42 +0000

    mido: Add lmkd props
     * Kang lmkd props from newest google devices (aka Pixels).
    
     * This makes lmkd spread its wings and become a multitasking beast over kernel side LMK.

--- prop.mk
+++ prop.mk
@@ -360,3 +360,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.qti.inputopts.enable=true \
     persist.vendor.qti.inputopts.movetouchslop=0.6 \
     ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0
+
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
