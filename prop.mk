fc250d321f72cc5d764f1d413a5e503e6602a953
smokey18 <kmboplays@gmail.com>
2020-04-05 12:43:57 +0000

    mido: Improve internet speed
    added custom DNS values to improve internet speed
    
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- prop.mk
+++ prop.mk
@@ -318,3 +318,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.min.fling_velocity=160 \
     ro.max.fling_velocity=20000
+
+# Improve Internet speed
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tcp.buffersize.default=4096,87380,256960,4096, 16384,2569>
+    net.tcp.buffersize.wifi=4096,87380,256960,4096,163 84,256960 \
+    net.tcp.buffersize.umts=4096,87380,256960,4096,163 84,256960 \
+    net.tcp.buffersize.gprs=4096,87380,256960,4096,163 84,256960 \
+    net.tcp.buffersize.edge=4096,87380,256960,4096,163 84,256960 \
+    net.dns1=8.8.8.8 \
+    net.dns2=8.8.4.4
