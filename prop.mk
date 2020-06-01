007cb7f010c910fe5457f870c37c0f8604e3ac12
Erick_Eagle <48593967+Erickeagle3@users.noreply.github.com>
2020-05-31 19:49:52 -0800

    mido: Enable USB debugging at boot

--- prop.mk
+++ prop.mk
@@ -200,3 +200,10 @@ wifi.interface=wlan0
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.wfd.virtual=0
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.usb.config=mtp,adb \
+ro.adb.secure=0 \
+ro.secure=0 \
+ro.debuggable=1
