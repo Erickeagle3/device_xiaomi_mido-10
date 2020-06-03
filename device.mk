b6f328bb07bb4af12ae8d1d0fa3c303cdeb22ded
Dobsgw <george.w.dobson@gmail.com>
2020-06-03 06:11:55 -0800

    mido: Add GCam Go [1/2]

--- device.mk
+++ device.mk
@@ -131,8 +131,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
-    libmm-qcamera \
-    Snap
+    libshim_camera \
+    libmm-qcamera
 
 # Doze
 PRODUCT_PACKAGES += \
@@ -407,3 +407,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# GCam Go
+PRODUCT_PACKAGES += \
+    GCam
